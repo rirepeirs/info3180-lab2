f2be429bcc31a968b676b8a405fd816424cb8616
rirepeirs <rirepeirs@gmail.com>
2025-02-26 22:04:10 -0500

    Added profile route, html and stylesheet

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+from datetime import datetime
 
 ###
 # Routing for your application.
@@ -17,6 +17,15 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile')
+def profile():
+    user_profile = {
+        "full_name": "Rianna Pearson", "username": "ripea",
+        "location": "Mandeville, Jamaica", "join_date": format_date_joined("2018-01-24"),
+        "bio":"Network Engineer. I am a young and aspiring network engineer with interest in cybersecurity. Please contact me if you would like to work together on a new project.",
+        "posts": 7, "following":20, "followers":50, "profile_pic": url_for('static', filename='woman.png')
+    }
+    return render_template('profile.html', user=user_profile)
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -45,3 +54,7 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined(date_string):
+    date_object = datetime.strptime(date_string, "%Y-%m-%d")
+    return date_object.strftime("%B, %Y")
